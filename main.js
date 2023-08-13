2644785cc7cbbb03e48f853abeab014cbd41ee00
Karina Pliego <karinapliegovergara@gmail.com>
2023-08-13 17:37:05 -0500

    Completed Simple Liker App

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById('modal');
+errorModal.classList.add('hidden');
 
+let likeButtons = document.querySelectorAll('.like');
 
-
+for (e of likeButtons) {
+  e.addEventListener("click", function likeFunction(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function() {
+        if (heart.innerText === EMPTY_HEART) {
+          //then we have to change from empty heart to full heart
+          heart.innerText = FULL_HEART;
+          heart.className = "activated-heart";
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.className = '';
+        }
+      })
+      .catch(function(error) {
+        const modal = document.getElementById("modal");
+        modal.className = '';
+        modal.innerText = error;
+        setTimeout(function() {
+          modal.className = "hidden"
+        }, 3000);
+      });
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
